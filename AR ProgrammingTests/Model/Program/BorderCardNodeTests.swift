1b6082e4df244d108858f8ce72f931eef4f0de45
Emil Nielsen <emilqz@gmail.com>
2019-03-12 11:53:48 +0100

    Added tests for LoopCardNode

---
+++ AR ProgrammingTests/Model/Program/BorderCardNodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  BorderCardNodeTests.swift
+//  AR Programming
+//  
+//  Created by Emil Nielsen on 11/03/2019.
+//  Copyright © 2019 Emil Nielsen and Kasper Schultz Davidsen. All rights reserved.
+//
+
+import XCTest
+import simd
+@testable import AR_Programming
+
+class BorderCardNodeTests: XCTestCase {
+
+//findLoopCard
+//next
+}
