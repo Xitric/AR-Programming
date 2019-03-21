16287b330f7b91b7dc819366ef22bf30183f7a44
Kasper Schultz Davidsen <KasperSchultzDavidsen@gmail.com>
2019-03-21 10:51:45 +0100

    Added missing card resources

--- AR Programming/Model/Program/Cards/ParameterCard.swift
+++ AR Programming/Model/Program/Cards/ParameterCard.swift
@@ -12,14 +12,16 @@ import SceneKit
 struct ParameterCard: Card {
     
     let name = "Tal"
-    let internalName = "parameter"
+    var internalName: String {
+        return "param\(parameter)"
+    }
     let summary = "Tal for dit program."
     let description: String
     var parameter: Int
     
     init(paremeter: Int) {
         self.parameter = paremeter
-        self.description = "Brug dette kort til at fortælle hvor mange gange et loop skal gentage sig. Dette tal kortet \(parameter) skal sættes ovenfor eller under et gentagelses kort."
+        self.description = "Brug dette kort til at fortælle, at et gentagelseskort skal gentage sig \(parameter) gange. Dette talkort skal placeres over eller under et gentagelseskort."
     }
     
     func getAction(forEntity entity: Entity) -> ActionComponent? {
