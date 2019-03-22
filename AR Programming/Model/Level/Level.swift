76c38551864a287216c3eec60cec8c2d54d6b71d
Kasper Schultz Davidsen <KasperSchultzDavidsen@gmail.com>
2019-03-22 09:42:11 +0100

    Added static props to levels

--- AR Programming/Model/Level/Level.swift
+++ AR Programming/Model/Level/Level.swift
@@ -83,8 +83,24 @@ class Level: Decodable, UpdateDelegate {
         name = try container.decode(String.self, forKey: .name)
         levelNumber = try container.decode(Int.self, forKey: .number)
         unlocks = try? container.decode(String.self, forKey: .unlocks)
-        
         entityManager = EntityManager()
+        
+        let props = try container.decode([PropJSON].self, forKey: .props)
+        for prop in props {
+            addProp(fromJson: prop)
+        }
+    }
+    
+    private func addProp(fromJson propJson: PropJSON) {
+        let prop = Entity()
+        
+        let transform = TransformComponent(location: simd_double3(propJson.x, 0, propJson.y))
+        prop.addComponent(transform)
+        
+        let resource = ResourceComponent(resourceIdentifier: propJson.resourceIdentifier)
+        prop.addComponent(resource)
+        
+        entityManager.addEntity(prop)
     }
     
     private enum CodingKeys: String, CodingKey {
@@ -92,7 +108,14 @@ class Level: Decodable, UpdateDelegate {
         case name
         case number
         case unlocks
+        case props
     }
+}
+
+struct PropJSON: Decodable {
+    let x: Double
+    let y: Double
+    let resourceIdentifier: String
 }
 
 protocol LevelDelegate: class {
