b1506255d84a20c9e369ea60ac8122de1f829f01
Emil Nielsen <emilqz@gmail.com>
2019-03-22 13:10:35 +0100

    Started press cards

---
+++ AR Programming/View/CardRect.swift
@@ -0,0 +1,9 @@
+//
+//  CardRect.swift
+//  AR Programming
+//  
+//  Created by Emil Nielsen on 21/03/2019.
+//  Copyright © 2019 Emil Nielsen and Kasper Schultz Davidsen. All rights reserved.
+//
+
+import Foundation
